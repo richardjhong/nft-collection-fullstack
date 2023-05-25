98265cccbc7c698c0815b6b762460c581722f9e2
Richard Hong <rhong24@gmail.com>
2023-05-25 15:41:17 -0500

    modifying deploy.ts, modifying hardhat config, deploying contract

---
+++ hh-backend/constants/index.ts
@@ -0,0 +1,3 @@
+export const WHITELIST_CONTRACT_ADDRESS = "0xb2E3fc02fBeC342457E47845e60b4D7De37f2780";
+export const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
--- hh-backend/scripts/deploy.ts
+++ hh-backend/scripts/deploy.ts
@@ -1,24 +1,24 @@
 import { ethers } from "hardhat";
+import "dotenv/config";
+import { WHITELIST_CONTRACT_ADDRESS, METADATA_URL } from "../constants";
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+const main = async () => {
+  const whitelistContract = WHITELIST_CONTRACT_ADDRESS;
+  const metadataURL = METADATA_URL;
+  const cryptoDevsContract = await ethers.getContractFactory("CryptoDevs");
 
-  const lockedAmount = ethers.utils.parseEther("0.001");
-
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with ${ethers.utils.formatEther(lockedAmount)}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+  const deployedCryptoDevsContract = await cryptoDevsContract.deploy(
+    metadataURL,
+    whitelistContract
   );
-}
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+  await deployedCryptoDevsContract.deployed();
+  console.log("Crypto Devs Contract Address: ", deployedCryptoDevsContract.address);
+};
+
+main()
+  .then(() => process.exit(0))
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
