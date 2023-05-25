59d4e96841e80abf818c39695c85cc180e3948b0
Richard Hong <rhong24@gmail.com>
2023-05-25 18:14:54 -0500

    adding ethers and web3modal dependencies, replacing content of page.tsx, adding constants folder with abi and nft contract address

--- next-frontend/src/app/page.tsx
+++ next-frontend/src/app/page.tsx
@@ -1,113 +1,341 @@
-import Image from 'next/image'
+"use client";
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+import { Contract, providers, utils } from "ethers";
+import Head from "next/head";
+import React, { useEffect, useState, useRef } from "react";
+import Web3Modal from "web3modal";
+import { abi, NFT_CONTRACT_ADDRESS } from "../../constants";
+
+const Home = () => {
+  const [walletConnected, setWalletConnected] = useState<boolean>(false);
+  const [presaleStarted, setPresaleStarted] = useState<boolean>(false);
+  const [presaleEnded, setPresaleEnded] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [isOwner, setIsOwner] = useState<boolean>(false);
+  const [tokenIdsMinted, setTokenIdsMinted] = useState<string>("0");
+  const web3ModalRef = useRef<Web3Modal | undefined>(undefined);
+
+  /**
+   * presaleMint: Mint an NFT during the presale
+   */
+  const presaleMint = async () => {
+    try {
+      const signer = await getProviderOrSigner(true);
+      const nftContract = new Contract(NFT_CONTRACT_ADDRESS, abi, signer);
+      const tx = await nftContract.presaleMint({
+        value: utils.parseEther("0.01"),
+      });
+      setLoading(true);
+
+      await tx.wait();
+      setLoading(false);
+      window.alert("You successfully minted a Crypto Dev!");
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  /**
+   * publicMint: Mint an NFT after the presale
+   */
+  const publicMint = async () => {
+    try {
+      const signer = await getProviderOrSigner(true);
+      const nftContract = new Contract(NFT_CONTRACT_ADDRESS, abi, signer);
+      const tx = await nftContract.mint({
+        value: utils.parseEther("0.01"),
+      });
+      setLoading(true);
+
+      await tx.wait();
+      setLoading(false);
+      window.alert("You successfully minted a Crypto Dev!");
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  /**
+   * connectWallet: Connects the MetaMask wallet
+   */
+  const connectWallet = async () => {
+    try {
+      await getProviderOrSigner();
+      setWalletConnected(true);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  /**
+   * startPresale: Starts the presale for the NFT collection
+   */
+  const startPresale = async () => {
+    try {
+      const signer = await getProviderOrSigner(true);
+      const nftContract = new Contract(NFT_CONTRACT_ADDRESS, abi, signer);
+      const tx = await nftContract.startPresale();
+      setLoading(true);
+
+      await tx.wait();
+      setLoading(false);
+      await checkIfPresaleStarted();
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  /**
+   * checkIfPresaleStarted: Checks if the presale has started by quering the 'presaleStarted' variable in the contract
+   */
+  const checkIfPresaleStarted = async () => {
+    try {
+      const provider = await getProviderOrSigner();
+      const nftContract = new Contract(NFT_CONTRACT_ADDRESS, abi, provider);
+      const _presaleStarted = await nftContract.presaleStarted();
+
+      if (!_presaleStarted) await getOwner();
+
+      setPresaleStarted(_presaleStarted);
+      return _presaleStarted;
+    } catch (err) {
+      console.error(err);
+      return false;
+    }
+  };
+
+  /**
+   * checkIfPresaleEnded: Checks if the presale has ended by quering the 'presaleEnded' variable in the contract
+   */
+  const checkIfPresaleEnded = async () => {
+    try {
+      const provider = await getProviderOrSigner();
+      const nftContract = new Contract(NFT_CONTRACT_ADDRESS, abi, provider);
+      const _presaleEnded = await nftContract.presaleEnded();
+
+      // _presaleEnded is BigNumber, using lt instead of '<'
+      const hasEnded = _presaleEnded.lt(Math.floor(Date.now() / 1000));
+      hasEnded ? setPresaleEnded(true) : setPresaleEnded(false);
+      return hasEnded;
+    } catch (err) {
+      console.error(err);
+      return false;
+    }
+  };
+
+  /**
+   * getOwner: Calls the contract to retrieve the owner
+   */
+  const getOwner = async () => {
+    try {
+      const provider = await getProviderOrSigner();
+      const nftContract = new Contract(NFT_CONTRACT_ADDRESS, abi, provider);
+      const _owner = await nftContract.owner();
+
+      const signer = (await getProviderOrSigner(true)) as providers.JsonRpcSigner;
+      const address = await signer.getAddress();
+
+      if (address.toLowerCase() === _owner.toLowerCase()) setIsOwner(true);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  /**
+   * getTokenIdsMinted: Gets the number of tokenIds that have been minted
+   */
+  const getTokenIdsMinted = async () => {
+    try {
+      const provider = await getProviderOrSigner();
+      const nftContract = new Contract(NFT_CONTRACT_ADDRESS, abi, provider);
+      const _tokenIds = await nftContract.tokenIds();
+      // _tokenIds is a 'Big Number', converting to string
+      setTokenIdsMinted(_tokenIds.toString());
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  /**
+   * Returns a Provider or Signer object representing the Ethereum RPC with or without the
+   * signing capabilities of metamask attached
+   *
+   * A `Provider` is needed to interact with the blockchain - reading transactions, reading balances, reading state, etc.
+   *
+   * A `Signer` is a special type of Provider used in case a `write` transaction needs to be made to the blockchain, which involves the connected account
+   * needing to make a digital signature to authorize the transaction being sent. Metamask exposes a Signer API to allow your website to
+   * request signatures from the user using Signer functions.
+   *
+   * @param {*} needSigner - True if you need the signer, default false otherwise
+   */
+  const getProviderOrSigner = async (needSigner = false) => {
+    if (!web3ModalRef.current) {
+      throw new Error("web3ModalRef.current is undefined");
+    }
+
+    const provider = await web3ModalRef.current.connect();
+    const web3Provider = new providers.Web3Provider(provider);
+
+    const { chainId } = await web3Provider.getNetwork();
+    if (chainId !== 11155111) {
+      window.alert("Change the network to Sepolia");
+      throw new Error("Change the network to Sepolia");
+    }
+
+    if (needSigner) {
+      const signer = web3Provider.getSigner();
+      return signer;
+    }
+    return web3Provider;
+  };
+
+  useEffect(() => {
+    const connectAndCheckPresale = async () => {
+      if (!walletConnected) {
+        web3ModalRef.current = new Web3Modal({
+          network: "sepolia",
+          providerOptions: {},
+          disableInjectedProvider: false,
+        });
+        connectWallet();
+
+        // check if presale has started and ended
+        const _presaleStarted = await checkIfPresaleStarted();
+        if (_presaleStarted) {
+          checkIfPresaleEnded();
+        }
+
+        getTokenIdsMinted();
+
+        // Set an interval which gets called every 5 seconds to check presale has ended
+        const presaleEndedInterval = setInterval(async function () {
+          const _presaleStarted = await checkIfPresaleStarted();
+          if (_presaleStarted) {
+            const _presaleEnded = await checkIfPresaleEnded();
+            if (_presaleEnded) {
+              clearInterval(presaleEndedInterval);
+            }
+          }
+        }, 5 * 1000);
+
+        // set an interval to get the number of token Ids minted every 5 seconds
+        setInterval(async function () {
+          await getTokenIdsMinted();
+        }, 5 * 1000);
+      }
+    };
+
+    connectAndCheckPresale();
+  }, [walletConnected]);
+
+  /**
+   * renderbutton: Returns a button based on the state of the dApp
+   */
+  const renderButton = () => {
+    // If wallet is not connected, return a button which allows them to connect their wallet
+    if (!walletConnected) {
+      return (
+        <button
+          onClick={connectWallet}
+          className="rounded bg-blue-700 border-none text-white text-base p-5 w-52 cursor-pointer mb-2 md:w-full md:flex md:flex-col md:justify-center md:items-center"
+        >
+          Connect your wallet
+        </button>
+      );
+    }
+
+    // If we are currently waiting for something, return a loading button
+    if (loading) {
+      return (
+        <button className="rounded bg-blue-700 border-none text-white text-base p-5 w-52 cursor-pointer mb-2 md:w-full md:flex md:flex-col md:justify-center md:items-center">
+          Loading...
+        </button>
+      );
+    }
+
+    // If connected user is the owner, and presale hasn't started yet, allow them to start the presale
+    if (isOwner && !presaleStarted) {
+      return (
+        <button
+          onClick={startPresale}
+          className="rounded bg-blue-700 border-none text-white text-base p-5 w-52 cursor-pointer mb-2 md:w-full md:flex md:flex-col md:justify-center md:items-center"
+        >
+          Start Presale!
+        </button>
+      );
+    }
+
+    // If connected user is not the owner but presale hasn't started yet, tell them that
+    if (!presaleStarted) {
+      return (
+        <div>
+          <div className="text-lg">Presale hasn&#39;t started!</div>
+        </div>
+      );
+    }
+
+    // If presale started, but hasn't ended yet, allow for minting during the presale period
+    if (presaleStarted && !presaleEnded) {
+      return (
+        <div>
+          <div className="text-lg">
+            Presale has started!!! If your address is whitelisted, Mint a Crypto Dev 🥳
+          </div>
+          <button
+            onClick={presaleMint}
+            className="rounded bg-blue-700 border-none text-white text-base p-5 w-52 cursor-pointer mb-2 md:w-full md:flex md:flex-col md:justify-center md:items-center"
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            Presale Mint 🚀
+          </button>
+        </div>
+      );
+    }
+
+    // If presale started and has ended, it's time for public minting
+    if (presaleStarted && presaleEnded) {
+      return (
+        <button
+          onClick={publicMint}
+          className="rounded bg-blue-700 border-none text-white text-base p-5 w-52 cursor-pointer mb-2 md:w-full md:flex md:flex-col md:justify-center md:items-center"
+        >
+          Public Mint 🚀
+        </button>
+      );
+    }
+  };
+
+  return (
+    <div>
+      <Head>
+        <title>Crypto Devs</title>
+        <meta
+          name="description"
+          content="Whitelist-Dapp"
+        />
+        <link
+          rel="icon"
+          href="/favicon.ico"
+        />
+      </Head>
+      <div className="min-h-screen flex flex-row justify-center items-center font-mono">
+        <div className="mx-8">
+          <h1 className="text-4xl mb-2">Welcome to Crypto Devs!</h1>
+          <div className="text-lg">It&#39;s an NFT collection for developers in Crypto.</div>
+          <div className="text-lg">{tokenIdsMinted}/20 have been minted</div>
+          {renderButton()}
+        </div>
+        <div>
+          <img
+            className="w-70 h-50 ml-20"
+            src="./crypto-devs.svg"
+          />
         </div>
       </div>
+      <footer className="flex justify-center items-center py-8 border-t-2 border-gray-300">
+        Made with &#10084; by Crypto Devs
+      </footer>
+    </div>
+  );
+};
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default Home;
